a8cf2b236c33f2e25e02681f83ce84c437deec48
knika-starkey <knika787@gmail.com>
2022-10-09 13:04:34 +0300

    more piza

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ function orderingPizza(n = 0, count = 1) {
   count = count == 0 ? 1 : count;
   console.log(count);
   let str = "Ваше замовлення: ";
-  let sum = 0;
+  let sum = count;
   switch (n) {
     case 0:
       str += `Фірмова піца -`;
-      sum = count * 75;
+      sum *= 75;
       break;
     case 1:
       str += `Піца з грибами -`;
-      sum = count * 123;
+      sum *= 123;
       break;
     case 2:
       str += `Піца з куркою і ананасом -`;
-      sum = count * 150;
+      sum *= 150;
+      break;
+    case 3:
+      str += `Піца 'Маргарита' -`;
+      sum *= 65;
+      break;
+    case 4:
+      str += `Піца 'Чотири сири' -`;
+      sum *= 95;
+      break;
+    case 5:
+      str += `Bегетаріанська піца -`;
+      sum *= 95;
       break;
     default:
       str = "Вибачте, у нас нема такої піцци у меню";
@@ -26,7 +38,7 @@ function orderingPizza(n = 0, count = 1) {
   return str;
 }
 let order = +prompt(
-  "наприклад, 1 - піца з грибами, 2 - піца з куркою і ананасом"
+  "наприклад, 1 - піца з грибами, 2 - піца з куркою і ананасом, 3 - піца 'Маргарита', 4 - піца 'Чотири сири', 5 - вегетаріанська піца"
 );
 let countt = +prompt("Скільки піц?");
 alert(orderingPizza(order, countt));
